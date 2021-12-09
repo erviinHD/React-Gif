3b7d38f5b70db6bb7025dc624f4bc75504ccdf5f
ervin cabascango <ervin.cabascango.s@gmail.com>
2021-12-09 11:30:25 -0500

    feat: Add category for input text

--- src/GifApp.js
+++ src/GifApp.js
@@ -1,22 +1,22 @@
 import React, { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
 
 const GifApp = () => {
 
     //const categories = ['One Puch', 'Samurai X', 'Dragon Ball']
     const [categories, setCategories] = useState(['One Puch', 'Samurai X', 'Dragon Ball']);
 
-    const handleAdd = () => {
-        setCategories([...categories, 'Demon Souls'])
-        //setCategories(cats=>[...cats, 'Demon Souls'])
-
-    }
+    //const handleAdd = () => {
+    //    setCategories([...categories, 'Demon Souls'])
+    //    //setCategories(cats=>[...cats, 'Demon Souls'])
+    //}
 
     return (
         <>
             <h2>Gif Expert App</h2>
             <hr></hr>
 
-            <button onClick={handleAdd}>Add Category</button>
+            <AddCategory setCategories={setCategories}/>
 
             <ol>
                 {categories.map((category) => {
