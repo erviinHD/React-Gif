698fec1a7d7b1da70b57d0f450cd13dacedc138a
ervin cabascango <ervin.cabascango.s@gmail.com>
2021-12-09 10:49:29 -0500

    feat: Add list of categories

--- src/GifApp.js
+++ src/GifApp.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 const GifApp = () => {
+
+    //const categories = ['One Puch', 'Samurai X', 'Dragon Ball']
+    const [categories, setCategories] = useState(['One Puch', 'Samurai X', 'Dragon Ball']);
+
+    const handleAdd = () => {
+        setCategories([...categories, 'Demon Souls'])
+        //setCategories(cats=>[...cats, 'Demon Souls'])
+
+    }
+
     return (
         <>
             <h2>Gif Expert App</h2>
             <hr></hr>
+
+            <button onClick={handleAdd}>Add Category</button>
+
+            <ol>
+                {categories.map((category) => {
+                    return <li key={category}> {category} </li>
+                })}
+            </ol>
         </>)
 }
 
