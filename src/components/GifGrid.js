8beda1cb2e5ee3259cf904c127f0fc1b4ab195bb
ervin cabascango <ervin.cabascango.s@gmail.com>
2021-12-09 15:52:35 -0500

    feat: Add helpers by api

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,31 +1,33 @@
 import React, { useEffect, useState } from 'react'
 import { GifGridItems } from './GifGridItems';
+import { getGifs } from "../helpers/getGifs";
 
 export const GifGrid = ({ category }) => {
 
-    const [images, setimages] = useState([])
+    const [images, setImages] = useState([])
 
     //useEfect
     useEffect(() => {
-        getGifs();
-    }, [])
+        getGifs(category)
+            .then(setImages)
+    }, [category])
 
-    const getGifs = async () => {
-        const URL = 'https://api.giphy.com/v1/gifs/search?q=starwars&limit=10&api_key=7JdRpgPr7cMfns4tjwTV2lDVg083GCvf'
-        const resp = await fetch(URL);
-        const { data } = await resp.json();
+    // const getGifs = async () => {
+    //     const URL = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=7JdRpgPr7cMfns4tjwTV2lDVg083GCvf`
+    //     const resp = await fetch(URL);
+    //     const { data } = await resp.json();
 
-        const gifs = data.map(img => {
-            return {
-                id: img.id,
-                title: img.title,
-                url: img.images?.downsized_medium.url
-            }
-        })
+    //     const gifs = data.map(img => {
+    //         return {
+    //             id: img.id,
+    //             title: img.title,
+    //             url: img.images?.downsized_medium.url
+    //         }
+    //     })
 
-        console.log(gifs);
-        setimages(gifs)
-    }
+    //     console.log(gifs);
+    //     setimages(gifs)
+    // }
 
     //getGifs();
 
