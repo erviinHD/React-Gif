a464f4620aea30a264672dd72cbdceb068789421
ervin cabascango <ervin.cabascango.s@gmail.com>
2021-12-09 10:29:14 -0500

    feat: Init proyect GifExpert

---
+++ src/GifApp.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const GifApp = () => {
+    return (
+        <>
+            <h2>Gif Expert App</h2>
+            <hr></hr>
+        </>)
+}
+
+export default GifApp;
